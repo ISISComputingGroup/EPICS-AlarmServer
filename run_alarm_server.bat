6835aad436ddd0e7b8e12027d6792badf642510c
Kris Ward <kris.ward@stfc.ac.uk>
2014-08-06 08:12:43 +0000

    #474 - Added alarm database configuration files.
    Former-commit-id: 56067b5b841dddffd8d3d8853a6345c1d6cfa6f9

--- run_alarm_server.bat
+++ run_alarm_server.bat
@@ -1,1 +1,9 @@
-start AlarmServer.exe -root test4 -pluginCustomization alarm_server_settings.ini
+@echo off
+
+REM get root configuration name from ini file
+set CONFIG_NAME=
+for /F %%i in ( %MYDIRALARM%config_name.ini ) DO set CONFIG_NAME=%%i
+
+
+@echo Starting Alarm Server (Root Configuration: %CONFIG_NAME%)
+start AlarmServer.exe -root %CONFIG_NAME% -pluginCustomization alarm_server_settings.ini
